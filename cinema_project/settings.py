0e08b84a5b0222056656a8538f00ff94271afeb2
questras <michu123qwe@gmail.com>
2021-01-26 11:33:31 +0100

    test debug

--- cinema_project/settings.py
+++ cinema_project/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
+print(DEBUG)
 
 ALLOWED_HOSTS = ['star-cinema.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
